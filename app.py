27679d8ad28adf5f210bfad1f6c656c59af9ceaf
Sumit <sumitadikari2@gmail.com>
2025-07-31 11:32:16 +0530

    Add model and dataset files

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def predict():
                            recommended_domains=recommended_domains)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
